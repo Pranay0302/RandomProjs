1d560326480e82cadba4e5e95c700d05f598fbe1
Pranay Kumar Andra <pka.pranayandra02@gmail.com>
2020-11-12 04:41:42 +0000

    added reset button

--- ttt/index.js
+++ ttt/index.js
@@ -6,7 +6,7 @@ function move(event) {
     var change0 = document.getElementById("change");
     var turn = change0.innerHTML[0];
     // console.log(squareId);
-    // // console.log(turn);
+    // console.log(turn);
 
     if (square.innerHTML === "" && turn === "O") {
         square.innerHTML = "O";
@@ -30,44 +30,69 @@ function compare() {
     var s8 = document.getElementById("square-8").innerHTML;
     var s9 = document.getElementById("square-9").innerHTML;
 
-    //     // let streak = [
+    // let streak = [
 
-    //     //     [s1, s2, s3],
-    //     //     [s4, s5, s6],
-    //     //     [s7, s8, s9],
-    //     //     [s1, s4, s7],
-    //     //     [s2, s5, s8],
-    //     //     [s3, s6, s9],
-    //     //     [s1, s5, s9],
-    //     //     [s3, s5, s7],
+    //     [s1, s2, s3],
+    //     [s4, s5, s6],
+    //     [s7, s8, s9],
+    //     [s1, s4, s7],
+    //     [s2, s5, s8],
+    //     [s3, s6, s9],
+    //     [s1, s5, s9],
+    //     [s3, s5, s7],
 
-    //     // ]
+    // ]
 
     if (s1 === s2 && s2 === s3 && s3 === s1 && s1 !== "" && s2 !== "" && s3 !== "") {
         change0.innerHTML = "WINNER";
+        alert("winner");
     }
     if (s4 === s5 && s5 === s6 && s4 === s6 && s4 !== "" && s5 !== "" && s6 !== "") {
         change0.innerHTML = "WINNER";
+        alert("winner");
     }
     if (s7 === s8 && s8 === s9 && s7 === s9 && s7 !== "" && s8 !== "" && s9 !== "") {
         change0.innerHTML = "WINNER";
+        alert("winner");
     }
     if (s1 === s4 && s4 === s7 && s1 === s7 && s1 !== "" && s4 !== "" && s7 !== "") {
         change0.innerHTML = "WINNER";
+        alert("winner");
     }
     if (s2 === s5 && s5 === s8 && s2 === s8 && s2 !== "" && s5 !== "" && s8 !== "") {
         change0.innerHTML = "WINNER";
+        alert("winner");
     }
     if (s3 === s6 && s6 === s9 && s3 === s9 && s3 !== "" && s6 !== "" && s9 !== "") {
         change0.innerHTML = "WINNER";
+        alert("winner");
     }
     if (s1 === s5 && s5 === s9 && s1 === s9 && s1 !== "" && s5 !== "" && s9 !== "") {
         change0.innerHTML = "WINNER";
+        alert("winner");
     }
     if (s3 === s5 && s5 === s7 && s3 === s7 && s3 !== "" && s5 !== "" && s7 !== "") {
         change0.innerHTML = "WINNER";
+        alert("winner");
     }
 
+}
+
+function clear() {
+    document.getElementById("square-1").innerHTML = "";
+    document.getElementById("square-2").innerHTML = "";
+    document.getElementById("square-3").innerHTML = "";
+    document.getElementById("square-4").innerHTML = "";
+    document.getElementById("square-5").innerHTML = "";
+    document.getElementById("square-6").innerHTML = "";
+    document.getElementById("square-7").innerHTML = "";
+    document.getElementById("square-8").innerHTML = "";
+    document.getElementById("square-9").innerHTML = "";
+
+    document.getElementById("change").innerHTML = "X turn";
+}
 
 
+function new_game() {
+    clear();
 }
